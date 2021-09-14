90af85dad78be2fa9a42e0cffe4f13ed684f38b6
petumazo <davidalvarezmazzo@gmail.com>
2021-09-14 18:36:41 -0500

    Add clickable style.

--- src/utils/list-of-char.js
+++ src/utils/list-of-char.js
@@ -13,8 +13,8 @@ const renderCharacters = () => {
         <img src=${element.image} alt="${element.name} image" class="mx-auto px-4 pt-4 pb-2 w-75">
         <p class="text-center mb-3 bold">${element.name}</p>
             <div class="d-flex flex-column align-items-center gap-2">
-                <span class="char-btn w-50 px-4 py-2 border text-center bg-dark text-info">Comments</span>
-                <span class="char-btn w-50 px-4 py-2 border text-center bg-dark text-info">Reservations</span>
+                <span class="char-btn w-50 px-4 py-2 border text-center bg-dark text-info clickable">Comments</span>
+                <span class="char-btn w-50 px-4 py-2 border text-center bg-dark text-info clickable">Reservations</span>
             </div>
             
         `;
