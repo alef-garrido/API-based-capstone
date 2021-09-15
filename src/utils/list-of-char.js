c10a667a35a11fc4243cfdcfdccace5133236945
petumazo <davidalvarezmazzo@gmail.com>
2021-09-15 18:38:16 -0500

    Update HTML to work with new functionality.

--- src/utils/list-of-char.js
+++ src/utils/list-of-char.js
@@ -1,3 +1,5 @@
+import myRequestGet from './fetchData';
+
 const listOfChar = document.getElementById('list-of-char');
 
 const renderCharacters = async () => {
@@ -14,12 +16,23 @@ const renderCharacters = async () => {
         <img src=${element.image} alt="${element.name} image" class="mx-auto px-4 pt-4 pb-2 w-75">
         <p class="text-center mb-3 bold">${element.name}</p>
             <div class="d-flex flex-column align-items-center gap-2">
+                <div>
+                  <span>Likes</span>
+                  <span id="likes-char-${element.id}">0</span>
+                </div>
                 <span class="elementDisplay char-btn w-50 px-4 py-2 border text-center bg-dark text-info clickable">Comments</span>
                 <span class="char-btn w-50 px-4 py-2 border text-center bg-dark text-info clickable">Reservations</span>
             </div>
-            
         `;
       });
+
+      myRequestGet.getLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/likes')
+        .then((likesArray) => {
+          likesArray.forEach((item) => {
+            const likesChar = document.getElementById(`likes-char-${item.item_id}`);
+            likesChar.innerHTML = `${item.likes}`;
+          });
+        });
     });
 };
 
