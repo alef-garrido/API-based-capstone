bbbcab1e80ea8227d629f48175a83e44e7744512
petumazo <davidalvarezmazzo@gmail.com>
2021-09-15 22:01:08 -0500

    Add feature for adding a like when clicked.

--- src/utils/list-of-char.js
+++ src/utils/list-of-char.js
@@ -13,16 +13,17 @@ const renderCharacters = async () => {
         listOfChar.appendChild(li);
         li.id = element.id;
         li.innerHTML = `
-        <img src=${element.image} alt="${element.name} image" class="mx-auto px-4 pt-4 pb-2 w-75">
-        <p class="text-center mb-3 bold">${element.name}</p>
-            <div class="d-flex flex-column align-items-center gap-2">
-                <div>
-                  <span>Likes</span>
-                  <span id="likes-char-${element.id}">0</span>
-                </div>
-                <span class="elementDisplay char-btn w-50 px-4 py-2 border text-center bg-dark text-info clickable">Comments</span>
-                <span class="char-btn w-50 px-4 py-2 border text-center bg-dark text-info clickable">Reservations</span>
+          <img src=${element.image} alt="${element.name} image" class="mx-auto px-4 pt-4 pb-2 w-75">
+          <p class="text-center mb-1 bold">${element.name}</p>
+          <div class="d-flex flex-column align-items-center gap-2">
+            <div class="d-flex align-items-center justify-content-center w-100">
+              <i id="like-btn-${element.id}" class="far fa-heart me-1 clickable like-btn"></i>
+              <span>Likes</span>
+              <span id="likes-char-${element.id}">: 0</span>
             </div>
+            <span class="elementDisplay char-btn w-50 px-4 py-2 border text-center bg-dark text-info clickable">Comments</span>
+            <span class="char-btn w-50 px-4 py-2 border text-center bg-dark text-info clickable">Reservations</span>
+          </div>
         `;
       });
 
@@ -30,9 +31,24 @@ const renderCharacters = async () => {
         .then((likesArray) => {
           likesArray.forEach((item) => {
             const likesChar = document.getElementById(`likes-char-${item.item_id}`);
-            likesChar.innerHTML = `${item.likes}`;
+            likesChar.innerHTML = `: ${item.likes}`;
           });
         });
+      return result;
+    })
+    .then((result) => {
+      result.forEach((element) => {
+        const likeBtn = document.getElementById(`like-btn-${element.id}`);
+        const updateCount = document.getElementById(`likes-char-${element.id}`);
+        likeBtn.addEventListener('click', () => { 
+          myRequestGet.postLike('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/likes', element.id).then(() => myRequestGet.getLikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/likes').then((likesArray) => {
+            likesArray.forEach((item) => {
+              const likesChar = document.getElementById(`likes-char-${item.item_id}`);
+              likesChar.innerHTML = `: ${item.likes}`;
+            });
+          }));
+        });
+      });
     });
 };
 
