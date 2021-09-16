12d233cbfa0420715fb0d636c98eeb97fc53c21d
Alef <oa.g11@icloud.com>
2021-09-16 13:55:15 -0500

    fix linter failed checks

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,24 @@
 /* eslint-disable consistent-return */
 export default class myRequestGet {
   constructor() {
-    this.appID = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/';
-    this.submitComment = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/comments';
+    this.appID =
+      "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/";
+    this.submitComment =
+      "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/comments";
   }
 
-   static getAll = async (url, member = null) => {
-     try {
-       const charsData = await fetch(url)
-         .then((res) => res.json())
-         .then((data) => data);
-       const charArr = charsData[member];
+  static getAll = async (url, member = null) => {
+    try {
+      const charsData = await fetch(url)
+        .then((res) => res.json())
+        .then((data) => data);
+      const charArr = charsData[member];
 
-       return charArr;
-     } catch (err) {
-       console.error(err);
-     }
-   }
+      return charArr;
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   static getOne = async (url) => {
     const charsData = await fetch(url)
@@ -25,28 +27,33 @@ export default class myRequestGet {
       .catch((err) => console.error(err));
 
     return charsData;
-  }
+  };
 
   static getComments = async (id) => {
-    const comments = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/comments?item_id=${id}`)
+    const comments = await fetch(
+      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/comments?item_id=${id}`
+    )
       .then((response) => response.json())
       .then((response) => response)
       .catch((err) => err);
 
     return comments;
-  }
+  };
 
   static postComments = async (id, user, text) => {
-    await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/comments', {
-      method: 'POST',
-      body: JSON.stringify({
-        item_id: id,
-        username: user,
-        comment: text,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-  }
+    await fetch(
+      "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KUcXNh15Xp4XOwHZwJel/comments",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          item_id: id,
+          username: user,
+          comment: text,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+  };
 }
