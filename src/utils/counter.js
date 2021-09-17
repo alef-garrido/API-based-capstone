771b836a696082091aa501f14f425af1d9fce253
Alef <oa.g11@icloud.com>
2021-09-17 11:50:48 -0500

    Update fix for module and style

--- src/utils/counter.js
+++ src/utils/counter.js
@@ -1,8 +1,12 @@
-const itemsCounter = (data) => {
-  const count = data.length;
-  const number0fChars = document.getElementById('number-of-chars');
-  number0fChars.innerHTML = `${count}`;
-  return count;
+import myRequestGet from './fetchData';
+
+const number0fChars = document.getElementById('number-of-chars');
+
+const itemsCounter = async () => {
+  myRequestGet.getAll('https://rickandmortyapi.com/api/character', 'results')
+    .then((data) => {
+      number0fChars.innerHTML = `${data.length}`;
+    });
 };
 
 export default itemsCounter;
