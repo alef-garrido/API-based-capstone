d51cefed6d20700f88fa6132641b1da6d7a5c5cb
petumazo <davidalvarezmazzo@gmail.com>
2021-09-17 08:59:22 -0500

    Remove unnecesary test.

--- src/spec/index.test.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import Task from '../task';
-
-it('works', () => { });
--- src/task.js
+++
@@ -1,7 +0,0 @@
-export default class Task {
-  constructor(description = '', index = 0, completed = false) {
-    this.description = description;
-    this.index = index;
-    this.completed = completed;
-  }
-}
